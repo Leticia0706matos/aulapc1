06f43490f731f3da561b2e6d0e1945d9ab185fb1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-29 20:25:49 -0300

    programa

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("-----------------------------");
+Console.WriteLine("---- Carrinho de Compras ----");
+Console.WriteLine("-----------------------------");
+
+Produto produtoUm = new Produto();
+
+produtoUm.Preco = 40;
+produtoUm.Descricao = "Camisa P";
+produtoUm.Codigo = 1;
+
+Produto produtoDois = new Produto();
+
+produtoDois.Preco = 45;
+produtoDois.Descricao = "Camisa M";
+produtoDois.Codigo = 2;
+
+List<Produto> produtosDisponiveis = new List<Produto>();
+produtosDisponiveis.Add(produtoUm);
+produtosDisponiveis.Add(produtoDois);
+
+ExibirListaDeProdutos(produtosDisponiveis);
+
+void ExibirListaDeProdutos(List<Produto> produtos)
+{
+    Console.WriteLine($"| Código | Descrição   | Valor unitário |");
+    Console.WriteLine($"| ------ | ----------- | -------------- |");
+
+    produtos.ForEach(produto => {
+        Console.WriteLine($"| {produto.Codigo}      | {produto.Descricao}    | {produto.Preco:C2}       |");    
+    });
+
+    // for (int i = 0; i < produtosDisponiveis.Count; i++)
+    // {
+    //     Produto produto = produtosDisponiveis[i];
+    //     Console.WriteLine($"| {produto.Codigo}   | {produto.Descricao}  | {produto.Preco:C2} |");    
+    // }
+
+    // foreach(var produto in produtos)
+    // {
+    //     Console.WriteLine($"| {produto.Codigo}   | {produto.Descricao}  | {produto.Preco:C2} |");    
+    // } 
+}
